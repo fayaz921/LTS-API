f81a13e1994224876b78a1e4a3f93f07b2cd7273
DaaDu123 <saadmuhammad19115@gmail.com>
2026-04-05 22:00:44 +0500

    hotfix: remove the Base Entity

--- LTS/LTS.API/Domain/Entities/Department.cs
+++ LTS/LTS.API/Domain/Entities/Department.cs
@@ -1,11 +1,14 @@
 ﻿namespace LTS.API.Domain.Entities
 {
-    public class Department : BaseEntity
+    public class Department
     {
+        public Guid Id { get; set; }
         public string DepartmentName { get; set; } = string.Empty;
         public string? AddressContact { get; set; }
         public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
 
+        // Navigation
         public ICollection<Case> Cases { get; set; } = new List<Case>();
     }
 }
