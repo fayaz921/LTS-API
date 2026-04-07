5c09a8117735eb8902a474265c962fb1ce75458a
DaaDu123 <saadmuhammad19115@gmail.com>
2026-04-07 10:03:38 +0500

    Departement Entity are complete

---
+++ LTS/LTS.API/Features/Departments/DepartmentsController.cs
@@ -0,0 +1,41 @@
+﻿using LTS.API.Features.Departments.Commands.CreateDepartment;
+using LTS.API.Features.Departments.Commands.DeleteDepartment;
+using LTS.API.Features.Departments.Commands.UpdateDepartment;
+using LTS.API.Features.Departments.Queries.GetAllDepartments;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LTS.API.Features.Departments.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class DepartmentsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public DepartmentsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    [HttpPost]
+    public async Task<Guid> Create(CreateDepartmentCommand command)
+    {
+        return await _mediator.Send(command);
+    }
+
+    [HttpPut]
+    public async Task<bool> Update(UpdateDepartmentCommand command)
+    {
+        return await _mediator.Send(command);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<bool> Delete(Guid id)
+    {
+        return await _mediator.Send(new DeleteDepartmentCommand(id));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok(await _mediator.Send(new GetAllDepartmentsQuery()));
+    }
+}
---
+++ LTS/LTS.API/Features/Departments/Queries/GetAllDepartmentsQuery.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+using LTS.API.Features.Departments.DTOs;
+
+namespace LTS.API.Features.Departments.Queries.GetAllDepartments;
+public record GetAllDepartmentsQuery() : IRequest<List<GetAllDepartmentsDto>>;
