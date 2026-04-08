8464abb7c6446374f8d1323abb86bbababefcc17
Muhammad Fayaz <mfayaz21703@gmail.com>
2026-04-08 21:31:31 +0500

    de

--- LTS/LTS.API/Features/CaseDocumentFeature/Commands/DeleteDocument/DeleteCaseDocumentCommand.cs
+++
@@ -1,7 +0,0 @@
-﻿using MediatR;
-
-namespace LTS.API.Features.CaseDocumentFeature.Commands.DeleteDocument
-{
-    public record DeleteCaseDocumentCommand(Guid Id):IRequest<string>;
-
-}
--- LTS/LTS.API/Features/CaseDocumentFeature/Commands/UploadDocument/UploadCaseDocumentCommand.cs
+++
@@ -1,6 +0,0 @@
-﻿using MediatR;
-
-namespace LTS.API.Features.CaseDocumentFeature.Commands.UploadDocument
-{
-    public record UploadCaseDocumentCommand(Guid caseId, IFormFile File, string? FileName, string? Remarks) : IRequest<string>;
-}
