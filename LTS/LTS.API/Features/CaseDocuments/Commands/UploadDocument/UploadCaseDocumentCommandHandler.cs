e2aaab0c77902fe87921942128bcb45112f79de8
Shoaib96978 <muhammadshoaob26@gmail.com>
2026-06-10 08:48:28 +0500

    bugfix: in case document a validation is missing
    - A validation is missing for teh file size

--- LTS/LTS.API/Features/CaseDocuments/Commands/UploadDocument/UploadCaseDocumentCommandHandler.cs
+++ LTS/LTS.API/Features/CaseDocuments/Commands/UploadDocument/UploadCaseDocumentCommandHandler.cs
@@ -6,25 +6,27 @@ using MediatR;
 
 namespace LTS.API.Features.CaseDocuments.Commands.UploadDocument
 {
-    public class UploadCaseDocumentCommandHandler:IRequestHandler<UploadCaseDocumentCommand, ApiResponse<string>>
+    public class UploadCaseDocumentCommandHandler : IRequestHandler<UploadCaseDocumentCommand, ApiResponse<string>>
     {
         private readonly AppDbContext context;
         private readonly ICloudinaryService cloudinaryService;
 
-        public UploadCaseDocumentCommandHandler(AppDbContext Context,ICloudinaryService cloudinaryService)
+        public UploadCaseDocumentCommandHandler(AppDbContext Context, ICloudinaryService cloudinaryService)
         {
             context = Context;
             this.cloudinaryService = cloudinaryService;
         }
- 
+
 
         public async Task<ApiResponse<string>> Handle(UploadCaseDocumentCommand request, CancellationToken cancellationToken)
         {
-            var uploadResult =await cloudinaryService.UploadFileAsync(request.File);
-            var caseDocument= request.Map(uploadResult);
+            var uploadResult = await cloudinaryService.UploadFileAsync(request.File);
+            if (!uploadResult.IsSuccess)
+                return ApiResponse<string>.Fail(uploadResult.Message,System.Net.HttpStatusCode.Conflict);
+            var caseDocument = request.Map(uploadResult);
             context.CaseDocuments.Add(caseDocument);
             await context.SaveChangesAsync(cancellationToken);
-            return ApiResponse<string>.Ok(default!,"File Uploaded SuccessFully");
+            return ApiResponse<string>.Ok(default!, "File Uploaded SuccessFully");
         }
     }
 }
